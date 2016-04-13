be63cdfd336ac9b38b74161ac825a180d6c20738
junkka <johan.junkka@gmail.com>
2016-04-13 16:53:02 +0200

    new coxme glance
    + remove dependencies
    + add examples

--- R/broom_coxme.R
+++ R/broom_coxme.R
@@ -8,13 +8,19 @@
 #' @export
 #' @import coxme
 #' @import broom
+#' @examples 
+#' library(broom)
+#' library(coxme)
+#' fit <- coxme(Surv(y, uncens) ~ trt + (1|center), eortc)
+#' tidy(fit)
+#' 
 
-tidy.coxme <- function(x, exponentiate = TRUE, ...){
+tidy.coxme <- function(x, exponentiate = FALSE, ...){
   beta <- x$coefficients
   nvar <- length(beta)
   nfrail <- nrow(x$var) - nvar
   nn <- c("estimate", "exp()", "std.error", "statistic", "p.value")
-  se <- sqrt(diag(x$var)[nfrail + 1:nvar])
+  se <- sqrt(diag(as.matrix(x$var))[nfrail + 1:nvar])
   z <- qnorm((1 + 0.95)/2, 0, 1)
   ret <- data.frame(
     "term" = names(beta),
@@ -41,21 +47,70 @@ tidy.coxme <- function(x, exponentiate = TRUE, ...){
 #' @param x coxme object
 #' @param ... other params
 #' @export
+#' @examples
+#' library(broom)
+#' library(coxme)
+#' fit <- coxme(Surv(y, uncens) ~ trt + (1|center), eortc)
+#' glance(fit)
+#' 
 
 
 glance.coxme <- function(x, ...){
   loglik <- x$loglik + c(0, 0, x$penalty)
-  chi1 <- 2 * diff(x$loglik[c(1, 2)])
-  chi1 <- 2 * diff(loglik[1:2])
-  chi2 <- 2 * diff(loglik[c(1, 3)])
-  ret <- rbind(c(round(chi1, 2), round(x$df[1], 2), signif(1 -
-                                                             pchisq(chi1, x$df[1]), 5), round(chi1 - 2 * x$df[1],
-                                                                                              2), round(chi1 - log(x$n[1]) * x$df[1], 2)), c(round(chi2,
-                                                                                                                                                   2), round(x$df[2], 2), signif(1 - pchisq(chi2, x$df[2]),
-                                                                                                                                                                                 5), round(chi2 - 2 * x$df[2], 2), round(chi2 - log(x$n[1]) *
-                                                                                                                                                                                                                           x$df[2], 2)))
-  dimnames(ret) <- list(c("Integrated loglik", " Penalized loglik"),
-                        c("Chisq", "df", "p", "AIC", "BIC"))
-  # finish_glance(ret, x)
-  ret
+  chi1 <- 2 * diff(loglik[1:2]) 
+  chi2 <- 2 * diff(loglik[c(1,3)])
+  temp0 <- as.list(c(
+    x$n[2], x$n[1],
+    round(chi1, 2), round(x$df[1], 2),
+    as.numeric(loglik[3]),
+    signif(1 - pchisq(chi1, x$df[1]), 5),
+    round(chi1 - 2 * x$df[1], 2),
+    round(chi1 - log(x$n[1]) * x$df[1], 2)))
+  names(temp0) <- c("n", "events" , "Chisq", "df", "logLik", "p", "AIC", "BIC")
+  
+  ## random effects
+  random <- VarCorr(x)
+  nrow <-  sapply(random, 
+                  function(x) if (is.matrix(x)) nrow(x) else length(x))
+  maxcol <-max(sapply(random,
+                      function(x) if (is.matrix(x)) 1 + ncol(x) else 2))
+  temp1 <- matrix(NA, nrow=sum(nrow), ncol=maxcol)
+  indx <- 0
+  for (term in  random) {
+    if (is.matrix(term)) {
+      k <- nrow(term)
+      nc <- ncol(term)
+      for (j in 1:k) {
+        temp1[j + indx, 1] <- sqrt(term[j,j])
+        temp1[j + indx, 2] <- term[j,j]
+        if (nc > j) {
+          indx2 <- (j + 1):nc
+          temp1[j + indx, 1 + indx2] <- term[j, indx2]
+        }
+      }
+    }
+    else {
+      k <- length(term)
+      temp1[1:k + indx, 1] <- sqrt(term)
+      temp1[1:k + indx, 2] <- term
+    }
+    indx <- indx + k
+  }
+  
+  indx <- cumsum(c(1, nrow))   # starting row of each effect
+  temp3 <- rep("", nrow(temp1))
+  temp3[indx[-length(indx)]] <- names(random)
+  xname <- unlist(lapply(random, 
+                         function(x) if (is.matrix(x)) dimnames(x)[[1]] else names(x)))
+ 
+  b <- as.vector(temp1)
+  names(b) <- paste(rep(c("random_variance", "random_sd"), each = length(b)/2), temp3, sep = "_")
+  
+  # groups
+  grps <- unlist(lapply(x$frail, length))
+  names(grps) <- paste0("random_n_", names(grps))
+  
+  ret <- as.list(c(temp0, grps, b))
+  ret <- as.data.frame(ret)
+  broom:::finish_glance(ret, x)
 }
